90a509a5c3eb7b0a330f8977c6eed2f52e75d16a
abhi123-brain <abhisheksaroj6934@gmail.com>
2026-07-04 17:50:25 +0000

    updated

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -31,7 +31,52 @@
 let someNumber = 33
 
 let stringnumber = String(someNumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//********* Operations **************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Abhishek"
+
+let str2 = " Rao"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let num1= 100
+num1++
+console.log(num1);
+
+
+
+
 
 
