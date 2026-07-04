b659a76b3f23a5e82055e3633abe16decfcd9470
abhi123-brain <abhisheksaroj6934@gmail.com>
2026-07-04 16:50:20 +0000

    Conversion of Datatype

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = "33abc"
+// let score = null
+// let score = true
+// let score = undefined
+//  let score = "Abhishek"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" =>33
+// "33abc" => Nan
+// "Abhishek" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhishek" = true
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
