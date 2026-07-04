d07e581252d873a7f22b0ae1af2a81be4c64d5a2
abhi123-brain <abhisheksaroj6934@gmail.com>
2026-07-04 16:27:27 +0000

    Datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+ "use strict" //treat all JS code as newer version
+//alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Abhishek")
+
+let name = "Abhishek"
+let age = 14
+let isLogged = false
+
+console.table([name,age,isLogged])
+
+// number => 2 to power 53
+// bigint
+// string => "",''
+// symbol => unique
+// boolean => true/false
+// null => standalone value(empty)
+// undefined
+
+console.log(typeof "Abhishek") //String
+console.log(typeof age)        //number
+console.log(typeof isLogged)   //boolean
+console.log(typeof null);      //object
+console.log(typeof undefined); //undefined
+
